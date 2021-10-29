3045a579bf56b9d7fbbd6157a7acb908ded78bbd
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2021-10-29 13:56:22 -0400

    added method to get nodename

--- mpi_utils.h
+++ mpi_utils.h
@@ -2,11 +2,12 @@
 #define __MPI_UTILS__
 
 
-int      mpi_init(int argc, char** argv);
-int      get_grank();
-int      get_lrank();
-int      get_gsize();
-int      get_lsize();
+int mpi_init(int argc, char** argv);
+int get_grank();
+int get_lrank();
+int get_gsize();
+int get_lsize();
+char get_nodename();
 MPI_Comm get_lcomm();
 
 #endif
